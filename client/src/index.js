b568e996743e2fcb3abbff69aaa6abda82c54418
PavloMylkus <ppavlo82@gmail.com>
2022-10-14 12:17:05 +0300

    test PWA

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AppContextProvider } from './context/appContext';
 import { BrowserRouter } from "react-router-dom";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -19,4 +20,6 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+serviceWorkerRegistration.register();
 reportWebVitals();
+
